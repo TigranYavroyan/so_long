cf81a7191634045a40b93cfc8f6dc48ffeaf1eab
Tigran Yavroyan <tyavroya@c1r4s5.42yerevan.am>
2024-05-20 14:03:59 +0400

    hamalya

--- srcs/moving.c
+++ srcs/moving.c
@@ -6,7 +6,7 @@
 /*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:57:33 by tyavroya          #+#    #+#             */
-/*   Updated: 2024/05/20 13:38:38 by tyavroya         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:01:20 by tyavroya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 static int	checker_coin_exit_counter(t_game *game, int x, int y)
 {
+// 	mlx_string_put(game->mlx, game->win, 100, 100, 0xFF000000, ft_itoa(game->coins_count));
+// 	mlx_string_put(game->mlx, game->win, 200, 100, 0xFF000000, ft_itoa(game->endgame));
 	if (game->map[game->player->row + x][game->player->col + y] == WALL)
 		return (0);
-	else if (game->map[game->player->row + x][game->player->col + y] == EXIT && !game->endgame)
+	else if (game->coins_count == 0 && game->map[game->player->row + x][game->player->col + y] == EXIT)
+		game->endgame = 1;
+	if (game->map[game->player->row + x][game->player->col + y] == EXIT && !game->endgame)
 		return (0);
 	if (game->map[game->player->row + x][game->player->col + y] == COIN)
 		--(game->coins_count);
